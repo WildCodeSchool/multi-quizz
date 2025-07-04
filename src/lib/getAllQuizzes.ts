cd5b4fc740edbb0f3c4574d9ae70d9d1d66e6e41
DAMIEN FONTAINE <zelia.kowalski@gmail.com>
2025-07-04 12:07:55 +0200

    "Corrected getAllQuizzes"

--- src/lib/getAllQuizzes.ts
+++ src/lib/getAllQuizzes.ts
@@ -2,11 +2,11 @@ import { quizMessages } from "@/data/responseMessages";
 import { apiRoutes } from "@/data/ROUTES";
 import { QuizListModel } from "@/model/QuizListModel";
 
-export async function getAllQuiz(): Promise<QuizListModel[]> {
+export async function getAllQuizzes(): Promise<QuizListModel[]> {
   const res = await fetch(apiRoutes.QUIZ, { method: "GET" });
   if (!res.ok) throw new Error(quizMessages.loadingErrorQuizzes);
   const data = await res.json();
   return data.quizzes.map(
-    (q: any) => new QuizListModel(q.id, q.title, q.picture, q.slug)
+    (q: QuizListModel) => new QuizListModel(q.id, q.title, q.picture, q.slug)
   );
 }
