db3107cc627358c077c5e5732d39361a357bb6d1
Christophe Godet <godet.chris59@gmail.com>
2025-07-10 14:58:37 +0200

    fix: correct PR fetch in lib module

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,10 @@
 import Link from "next/link";
 import styles from "./page.module.css";
-import { db } from "../lib/db";
-import { Quiz } from "../types";
-
-async function getQuizzes(): Promise<Quiz[]> {
-  try {
-    const [rows] = await db.query(
-      "SELECT id, title, picture, slug FROM quizzes"
-    );
-
-    return rows as Quiz[];
-  } catch (error) {
-    console.error("Erreur lors de la récupération des quiz :", error);
-
-    return [];
-  }
-}
+import { getAllQuizzes } from "@/lib/getAllQuizzes";
+import { QuizModel } from "@/model/QuizModel";
 
 export default async function Home() {
-  const quizzes: Quiz[] = await getQuizzes();
+  const quizzes: QuizModel[] = await getAllQuizzes();
 
   return (
     <div className={styles.backgroundImage}>
@@ -37,7 +23,6 @@ export default async function Home() {
                 </Link>
               </div>
             </div>
-
             <div className={styles.linkRight}>
               <div className={styles.linkAccount}>
                 <Link className={styles.Account} href="/account">
@@ -55,7 +40,7 @@ export default async function Home() {
 
         <section className={styles.ImgButtonQuiz}>
           {quizzes.length > 0 ? (
-            quizzes.map((quiz: Quiz) => (
+            quizzes.map((quiz) => (
               <Link href={`/quiz/${quiz.slug}`} key={quiz.id}>
                 <img src={quiz.picture} alt={quiz.title} />
               </Link>
