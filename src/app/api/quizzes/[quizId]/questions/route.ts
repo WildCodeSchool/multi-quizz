1fe0df6bb184c08f81c4ca577b451a9e026cff5f
DAMIEN FONTAINE <zelia.kowalski@gmail.com>
2025-07-17 10:27:19 +0200

    "Add dynamic question"

--- src/app/api/quizzes/[quizId]/questions/route.ts
+++ src/app/api/quizzes/[quizId]/questions/route.ts
@@ -1,18 +1,42 @@
 import { NextResponse } from "next/server";
 import { db } from "@/lib/db";
 import { quizMessages } from "@/data/responseMessages";
+import { QuestionModel } from "@/model/QuestionModel";
 
-export async function GET() {
-  try {
-    const [questions] = await db.query(
-      "SELECT id, question, quiz_id FROM Questions"
-    );
+export async function GET(
+  _req: Request,
+  context: { params: { quizId: string } }
+) {
+  const { quizId } = await context.params;
+  const quizIdInt = parseInt(quizId, 10);
 
-    return NextResponse.json(questions);
-  } catch (error) {
-    console.error("Error fetching questions:", error);
+  if (isNaN(quizIdInt)) {
     return NextResponse.json(
-      { message: quizMessages.errorServer },
+      { error: quizMessages.invalidQuizzId },
+      { status: 400 }
+    );
+  }
+  try {
+    const [rows] = await db.query(
+      "SELECT id, question, quiz_id, number FROM Questions WHERE quiz_id = ? ORDER BY number ASC LIMIT 1",
+      [quizIdInt]
+    );
+    const questions = Array.isArray(rows) ? (rows as QuestionModel[]) : [];
+
+    if (questions.length === 0) {
+      return NextResponse.json(
+        { error: quizMessages.notFoundQuestion },
+        { status: 404 }
+      );
+    }
+    return NextResponse.json({ question: questions[0] });
+  } catch (error) {
+    console.error(
+      "Erreur MySQL (GET /api/quizzes/[quizId]/questions) :",
+      error
+    );
+    return NextResponse.json(
+      { error: quizMessages.errorServer },
       { status: 500 }
     );
   }
